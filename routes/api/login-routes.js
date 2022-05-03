b3f1cc7395f9976348aaac3671ec994632e503c6
David Gardiner <dgardi@gmail.com>
2022-05-03 00:47:42 -0400

    save progress, added boilerplate for handlebars into server.js

--- routes/api/login-routes.js
+++ routes/api/login-routes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 
 
 router.get('/login', (req, res) => {
-    console.log('Login is working')
+    res.render('login')
 })
-    .then();
+    
 
 router.get('/onboard', (req, res) => {
     console.log('Onboard is working')
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const sequelize = require('./config/connection')
+const exphbs = require('express-handlebars');
+const routes = require('./routes/');
 const app = express();
+
+const hbs = exphbs.create({});
 
 // This is the table generation when we first run the server and the table doesn't exist yet. 
 // const tablegen = require('./models')
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Needed for handlebars
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars');
+
+app.use(routes);
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
