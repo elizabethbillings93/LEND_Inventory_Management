65e896bef4ec5847fb8711936267d46bbf1857bc
David Gardiner <dgardi@gmail.com>
2022-05-04 12:54:36 -0400

    working on product routes

--- controllers/api/products-routes.js
+++ controllers/api/products-routes.js
@@ -1,12 +1,54 @@
 const router = require('express').Router();
+const {Categories, Products} = require('../../models')
 const hnb = require('handlebars');
 const path = require ('path');
 
-console.log('Working');
-router.get('/', async(req,res) => {
-    res.render('login');
-    
+// The /api/products endpoint
+
+// Get All Products
+router.get('/products', (req, res) => {
+    Products.findAll({
+        include: [Categories],
+    })
+    .then((products => res.json(products))
+    .then(data => res.render('product', {data}))
+    .catch(err)
 });
+
+// Get One Product 
+router.get('products/:id'), (req,res) => {
+    Products.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [Categories],
+    })
+    .then((products) => res.json(products))
+    .then(data => res.render('product', {data}))
+    .catch(err)
+}
+
+// Update Product
+router.put('products/:id'), (req, res) => {
+    //update product data
+    Products.update(req.body, {
+        where: {
+            id: req.params.id
+        },
+    })
+    .then((product) => {
+        if (req.body.)
+    })
+
+}
+
+
+
+// console.log('Working');
+// router.get('/', async(req,res) => {
+//     res.render('login');
+    
+// });
 
 
 
