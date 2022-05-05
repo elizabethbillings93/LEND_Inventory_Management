6e18e1ac97b31410bddb09d6ee310933196e7472
David Gardiner <dgardi@gmail.com>
2022-05-05 14:23:56 -0400

    working on dairy routes and rendering to page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -33,11 +33,56 @@ router.get('/products', withAuth, async (req, res) => {
 
     const categoryData = await Categories.findAll()
 
-    const categories = categoryData.map((cataegory) => cataegory.get({plain: true}));
+    const categories = categoryData.map((category) => category.get({plain: true}));
     res.render('inventory', {categories})
     } catch (err) {
         res.status(500).json(err);
     }
+
+
+
+router.get('/meats', withAuth, async(req,res)=> {
+    try {
+        // Find the logged in user based on session ID
+        const employeeData = await Employee.findByPk(req.session.user_id,{
+            attributes: { exclude: ['password'] },           
+        });
+    const employee = employeeData.get({ plain: true});
+    res.render('meats', {
+        ...employee,
+        logged_in: true 
+    });
+
+    const categoryData = await Categories.findAll()
+
+    const categories = categoryData.map((cataegory) => cataegory.get({plain: true}));
+    res.render('meats', {categories})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/dairy', withAuth, async(req,res)=> {
+    try {
+        // Find the logged in user based on session ID
+        const employeeData = await Employee.findByPk(req.session.user_id,{
+            attributes: { exclude: ['password'] },           
+        });
+    const employee = employeeData.get({ plain: true});
+    res.render('dairy', {
+        ...employee,
+        logged_in: true 
+    });
+
+    const categoryData = await Categories.findAll()
+
+    const categories = categoryData.map((category) => category.get({plain: true}));
+    res.render('dairy', {categories})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 
 });
 
